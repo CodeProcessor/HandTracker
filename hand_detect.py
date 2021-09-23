a2358e3bc738c78c2cd241537600e5f2e9398e20
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-09-23 16:32:15 +0530

    Hand detect code added

---
+++ hand_detect.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+@Filename:    hand_tacker.py
+@Author:      dulanj
+@Time:        2021-09-23 15.44
+"""
+import cv2
+import mediapipe as mp
+
+# from HandCoords import Hands
+
+
+class HandDetector():
+    def __init__(self, draw=True):
+        self.draw = draw
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_drawing_styles = mp.solutions.drawing_styles
+        self.mp_hands = mp.solutions.hands
+
+    def detect(self, image):
+        hands = self.mp_hands.Hands(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5)
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        results = hands.process(image)
+
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            if self.draw:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    self.mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        self.mp_hands.HAND_CONNECTIONS,
+                        self.mp_drawing_styles.get_default_hand_landmarks_style(),
+                        self.mp_drawing_styles.get_default_hand_connections_style())
+
+        # hands = Hands(results)
+
+        return image, hands
+
