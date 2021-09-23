e96df88db470b3d5a6d9564ae6793096de1d4f69
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-09-23 20:48:48 +0530

    drawing function updated

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from state_machine import State
 
 def main():
     _frame_count = 0
+    _draw_count = 0
+    draw_click = ""
     detector = HandDetector()
     sound = Sound()
     draw = Drawing()
@@ -47,10 +49,21 @@ def main():
             if hand_movements.click_sm.get_state() == State.transit:
                 if hand_movements.click_sm.get_previous_state() == State.on:
                     sound.click()
-                    draw.write(out_image, 20, 100, f"Clack", color=(0, 0, 255), scale=2, thick=2)
+                    draw_click = "Clack"
+                    _draw_count = 3
                 elif hand_movements.click_sm.get_previous_state() == State.off:
                     sound.double_click()
-                    draw.write(out_image, 20, 100, f"Click", color=(0, 255, 0), scale=2, thick=2)
+                    draw_click = "Click"
+                    _draw_count = 3
+
+            if draw_click == "Clack":
+                if _draw_count > 0:
+                    _draw_count -= 1
+                    draw.write(out_image, 20, 100, f"Clack", color=(0, 0, 255), scale=2, thick=3)
+            elif draw_click == "Click":
+                if _draw_count > 0:
+                    _draw_count -= 1
+                    draw.write(out_image, 20, 100, f"Click", color=(0, 255, 0), scale=2, thick=3)
 
             cv2.imshow('MediaPipe Hands', out_image)
             char = cv2.waitKey(5)
